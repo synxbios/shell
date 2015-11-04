4cdf9d6d69357c51843e1031c1d4ec667166d415
baardl <bard.lind@gmail.com>
2015-11-04 15:21:16 +0100

    Listing domains.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # shell-client
 Communicate to the Synxbios via Synx commands
+
+
+# Usage
+
+* ringo
+* var synx = require('./synx-shell');
+* var ls = synx.login('<username>','<password>');
+* synx.ls()
+  [ 'altranName',
+    'OsloAirport',
+    'Paal',
+    'BardLind',
+    'SynxDemo' ]
+  >> 
+
--- synx-shell.js
+++ synx-shell.js
@@ -4,6 +4,7 @@
 var httpclient = require('ringo/httpclient');
 var content = [];
 var pcc = [];
+var ls = [];
 exports.connect = function(username, password) {
     var url = 'https://api.ipify.org?format=json';
     var result = httpclient.get(url);
@@ -13,5 +14,35 @@ exports.connect = function(username, password) {
 };
 
 exports.ls = function() {
-    return pcc.ip;
+    var domains = [];
+    Object.keys(ls).forEach(function(key) {
+        //console.log(key, ls[key].id);
+        domains.push(ls[key].name);
+    });
+
+    return domains;
 }
+exports.login = function(username, password) {
+    var {request} = require('ringo/httpclient');
+    var exchange = request({
+        method: 'GET',
+        contentType: "application/json",
+        data: "application/json",
+        url: 'http://localhost:8020/domainconfig/domains',
+        username: username,
+        password: password,
+        headers: {
+            'x-custom-header': 'foobar'
+        }
+    });
+    if(exchange.status == 200) {
+        pcc = 'domains'; //exchange.content;
+        ls = JSON.parse(exchange.content);
+        //console.log("Ls" + ls);
+        return "loged in ok";//ls;
+    } else {
+        console.error("Failed to list domains. Status: " + exchange.status);
+    }
+}
+
+exports.do
