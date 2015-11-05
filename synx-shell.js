59283b84fd4f407e35f3fa2a6efd2611d10d9779
baardl <bard.lind@gmail.com>
2015-11-05 13:53:39 +0100

    Listing services!

--- synx-shell.js
+++ synx-shell.js
@@ -3,7 +3,7 @@
  */
 var httpclient = require('ringo/httpclient');
 var content = [];
-var pcc = [];
+//var pcc = [];
 var context = [];
 context.type = 'Unauthorized';
 var ls = [];
@@ -11,37 +11,78 @@ exports.connect = function(username, password) {
     var url = 'https://api.ipify.org?format=json';
     var result = httpclient.get(url);
     content = JSON.parse(result.content);
-    pcc = content;
+    //pcc = content;
     return content;
 };
 
 var domains = [];
-function logArrayElements(element, index, array) {
+var serviceNames = [];
+var credentials = [{'username':'',
+    'password':'',
+    'userTokenId':''}];
+
+function updateDomainIds(element, index, array) {
   console.log('a[' + index + '] = ' + element.domainId);
   domains.push(element.domainId);
 }
 
+function updateServiceNames(element, index, array) {
+    console.log('a[' + index + '] = ' + element.name);
+    serviceNames.push(element.name);
+}
 
-
-exports.login = function(username, password) {
+function listServices(domain){
+    console.debug('List services. Domain: ' + domain + ', username: ' + credentials.username);
     var {request} = require('ringo/httpclient');
     var exchange = request({
         method: 'GET',
         contentType: "application/json",
         data: "application/json",
-        url: 'http://localhost:8020/domainconfig/domains',
-        username: username,
-        password: password,
+        url: 'http://localhost:8020/domainconfig/domains/' + domain + '/services',
+        username: credentials.username,
+        password: credentials.password,
         headers: {
             'x-custom-header': 'foobar',
             'Accept': 'application/json'
         }
     });
     if(exchange.status == 200) {
-        pcc = 'domains'; //exchange.content;
+        //pcc = 'domains'; //exchange.content;
         console.log("Content: " + exchange.content);
         ls = JSON.parse(exchange.content);
-        ls.forEach(logArrayElements);
+        serviceNames = [];
+        ls.forEach(updateServiceNames);
+        console.log("serviceNames: " + serviceNames);
+        return serviceNames;
+    } else {
+        console.error("Failed to list domains. Status: " + exchange.status);
+    }
+
+}
+
+
+exports.login = function(username, password) {
+    credentials.username = username;
+    credentials.password = password;
+    var {request} = require('ringo/httpclient');
+    var exchange = request({
+        method: 'GET',
+        contentType: "application/json",
+        data: "application/json",
+        url: 'http://localhost:8020/domainconfig/domains',
+        username: credentials.username,
+        password: credentials.password,
+        headers: {
+            'x-custom-header': 'foobar',
+            'Accept': 'application/json'
+        }
+    });
+    if(exchange.status == 200) {
+        //pcc = 'domains'; //exchange.content;
+        console.log("Content: " + exchange.content);
+        ls = JSON.parse(exchange.content);
+        domains = [];
+        ls.forEach(updateDomainIds);
         console.log("domains: " + domains);
         context.type = "home";
         context.name = "home";
@@ -63,8 +104,16 @@ exports.ls = function() {
 
         return domains;
     } else if (context.type = "domain") {
-
+        console.log("List services.");
+        listServices(context.name);
+        return serviceNames;
+    } else if (context.type = "service") {
+        console.log("List items.");
     }
+}
+
+exports.cat = function(serviceName) {
+    return "TODO";
 }
 
 
@@ -72,10 +121,10 @@ exports.use = function(name) {
     console.log("context " + context.type);
     if (context.type == "home") {
         console.log("is home");
-        pcc.type = "domain";
-        pcc.name = name;
+        context.type = "domain";
+        context.name = name;
     }
-    return "- " +pcc.type + " : " + pcc.name ;
+    return exports.pcc();
 }
 
 exports.pcc = function() {
